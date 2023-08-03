7241c0608c29905593acff371de8a25ffd5dd397
zebiriabderrahim <77996269+zebiriabderrahim@users.noreply.github.com>
2023-08-03 07:58:21 -0400

    feat: add t command

--- commands.c
+++ commands.c
@@ -1,30 +1,36 @@
 #include "commands.h"
+#include <sys/fcntl.h>
 
-
-void copy(const char *src, const char *dest) {
+void copy(const char *src, const char *dest)
+{
     int fd_in = open(src, O_RDONLY);
-    if (fd_in == -1) {
+    if (fd_in == -1)
+    {
         perror("open source file failed");
         return;
     }
 
-    int fd_out = open(dest, O_WRONLY | O_CREAT | O_EXCL, 0644);
-    if (fd_out == -1) {
+    int fd_out = open(dest, COPY_DEFAULT_FLAGS, DEFAULT_PERMISSION);
+    if (fd_out == -1)
+    {
         perror("open destination file failed");
-        close(fd_in); // Cleanup: Close the input file descriptor
+        close(fd_in);
         return;
     }
 
     char buffer[BUFFER_SIZE];
     ssize_t bytes_read, bytes_written;
 
-    while ((bytes_read = read(fd_in, buffer, sizeof(buffer))) > 0) {
+    while ((bytes_read = read(fd_in, buffer, sizeof(buffer))) > 0)
+    {
         char *p = buffer;
-        while (bytes_read > 0) {
+        while (bytes_read > bytes_written)
+        {
             bytes_written = write(fd_out, p, bytes_read);
-            if (bytes_written == -1) {
+            if (bytes_written == -1)
+            {
                 perror("write failed");
-                close(fd_in);  
+                close(fd_in);
                 close(fd_out);
                 return;
             }
@@ -33,11 +39,52 @@ void copy(const char *src, const char *dest) {
         }
     }
 
-    if (bytes_read == -1) {
+    if (bytes_read == -1)
+    {
         perror("read failed");
     }
 
-    if (close(fd_in) == -1 || close(fd_out) == -1) {
+    if (close(fd_in) == -1 || close(fd_out) == -1)
+    {
         perror("close failed");
     }
+}
+
+static void write_all(int fd, char *buffer, ssize_t bytes_read) {
+    ssize_t bytes_written = write(fd, buffer, bytes_read);
+    if (bytes_written == -1) {
+        perror("write failed");
+        close(fd);
+        return;
+    }
+    if (bytes_written < bytes_read) {
+        write_all(fd, buffer + bytes_written, bytes_read - bytes_written);
+    }
+}
+
+
+void t(int argc, char *argv[])
+{
+    int fds[argc];
+    fds[0] = STDOUT_FILENO;
+    for (int i = 1; i < argc; i++)
+    {
+        fds[i] = open(argv[i], T_DEFAULT_FLAGS, DEFAULT_PERMISSION);
+        if (fds[i] == -1)
+        {
+            perror("open source file failed");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read, bytes_written;
+
+    while ((bytes_read = read(STDIN_FILENO, buffer, sizeof(buffer))) > 0)
+    {
+        for (size_t i = 0; i < argc; i++)
+            write_all(fds[i], buffer, bytes_read);
+    }
+    for (size_t i = 0; i < argc; i++)
+        close(fds[i]);
 }
--- commands.h
+++ commands.h
@@ -2,13 +2,18 @@
 #define COMMANDS_H
 
 #define BUFFER_SIZE 4096
+#define COPY_DEFAULT_FLAGS O_WRONLY | O_CREAT | O_EXCL
+#define T_DEFAULT_FLAGS   O_WRONLY | O_CREAT | O_TRUNC
+#define DEFAULT_PERMISSION 0644
+
 
 
 #include <unistd.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
-// Function prototype for the copy function
 void copy(const char *src, const char *dest);
+void t(int argc, char *argv[]);
 
 #endif // COMMANDS_H
